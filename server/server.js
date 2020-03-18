58db1e7460cbd771b6b7d42475438a1dce892a5c
Petrov Anton <gg4every@gmail.com>
2020-03-18 13:36:46 +0400

    [APP] Implement reverse proxy

--- server/server.js
+++ server/server.js
@@ -1,21 +1,29 @@
 import express from 'express';
 import compression from 'compression';
-import bodyParser from 'body-parser';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const root = path.resolve(__dirname, '..');
 const app = express();
+const api = {
+  target: `${process.env.API}`,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '/'
+  }
+};
 
 app.use(compression());
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
+app.use(createProxyMiddleware('/api', api));
 app.use(express.static(path.join(root, 'build')));
 app.get('*', (req, res) => res.sendFile(path.join(root, 'build', 'index.html')));
 
-// start app
-app.listen(80, (error) => {
+app.listen(process.env.PORT, (error) => {
   if (!error) {
-    console.log('Port 80');
+    console.log(`[express]: Running on port ${process.env.PORT}`);
   }
 });
 
