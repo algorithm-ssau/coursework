395c568ff569e688e57dc96bdd4df86cc15fbeb3
Anton Petrov <im@petrov.engineer>
2020-04-10 09:02:04 +0400

    [API] Implement error handling

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from starlette.exceptions import HTTPException
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+
+from .core.errors import http_422_error_handler, http_base_error_handler
 from .db.utils import close_connection, open_connection
 from .routes import api
 
@@ -7,6 +11,15 @@ APP = FastAPI()
 
 APP.add_event_handler("startup", open_connection)
 APP.add_event_handler("shutdown", close_connection)
+
+APP.add_exception_handler(
+    HTTPException,
+    http_base_error_handler
+)
+APP.add_exception_handler(
+    HTTP_422_UNPROCESSABLE_ENTITY,
+    http_422_error_handler
+)
 
 APP.include_router(
     api.ROUTER,
