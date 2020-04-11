0c65e058b9c0fbc3077719f6850b6b8aa7e733bd
Anton Petrov <im@petrov.engineer>
2020-04-11 05:31:09 +0400

    [APP] Add color constants

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+const white = '#fff';
+const black = '#212121';
+const peach = '#ffd8be';
+const pink = '#ea6676';
+const sand = '#f4a26a';
+
+export default {
+  white,
+  black,
+  peach,
+  pink,
+  sand
+};
