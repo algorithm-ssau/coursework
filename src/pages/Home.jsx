fef5ed1caf9346163772317464f4b8954fb530f0
Anton Petrov <im@petrov.engineer>
2020-04-19 08:19:21 +0400

    [APP] Use Router HOC for pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 const Home = () => (
   <div>Hello World!</div>
 );
 
-export default Home;
+export default withRouter(Home);
