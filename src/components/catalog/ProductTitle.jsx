bedd7086a5bfe3b53f70cc94362d5c8b3c8437ce
Vadim Tarasov <vtarasow163@gmail.com>
2020-06-01 06:16:00 +0400

    [APP] Add catalog components

---
+++ src/components/catalog/ProductTitle.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components/macro';
+
+import typography from '../../styles/typography';
+
+const ProductTitle = styled.strong`
+  ${typography.title}
+
+  display: block;
+
+  height: 72px;
+  margin-bottom: 20px;
+`;
+
+export default ProductTitle;
